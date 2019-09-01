06c947377c65c966b5a8293162498b08f1a8c2bb
KommentatorForAll <46420447+KommentatorForAll@users.noreply.github.com>
2019-09-01 11:41:50 +0200

    Delete pi.js

--- Fun/pi.js
+++
@@ -1,8 +0,0 @@
-const discord = require("discord.js");
-
-module.exports.name = "pi";
-module.exports.description = "Shows you a few decimals of pi";
-module.exports.usage = pre => {return `\`${pre}pi\``;};
-module.exports.ex = async (message, args, client) => {
-  message.channel.send("Pi is about 3.1415926535897932384626433832795028841971693993");
-}
