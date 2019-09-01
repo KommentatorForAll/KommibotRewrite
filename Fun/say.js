97965e7bf9cbc79a639358a2d01eaaee4f55bacd
KommentatorForAll <46420447+KommentatorForAll@users.noreply.github.com>
2019-09-01 11:42:31 +0200

    Delete say.js

--- Fun/say.js
+++
@@ -1,9 +0,0 @@
-const discord = require("discord.js");
-
-module.exports.name = "say";
-module.exports.description = "sais what you want it to say";
-module.exports.usage = pre => {return `\`${pre}say hello\``;};
-module.exports.args = ["<text>"];
-module.exports.ex = async (message, args, client) => {
-  message.channel.send(args.join(" "));
-}
